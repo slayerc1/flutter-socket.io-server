168dc94f96eb37bda303715f18f8abdd021e0039
Raul Rodriguez Olmo <r.rodriguez-olmo@wata.es>
2023-03-28 10:46:43 +0200

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,46 @@
-const {io} = require('../index');
+const { io } = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Héroes del Silencio'));
+bands.addBand(new Band('Metallica'));
 
 // Mensajes de Sockets
 io.on('connection', client => {
-    console.log('Cliente conectado');
-    client.on('disconnect', () => { 
-        console.log('Cliente desconectado');
-     });
+   console.log('Cliente conectado');
 
-     client.on('mensaje', (payload) => {
-        console.log('Mensaje: ', payload);
+   client.emit('active-bands', bands.getBands());
 
-        io.emit('mensaje', { admin: 'Nuevo mensaje' });
-     });
+   client.on('disconnect', () => {
+      console.log('Cliente desconectado');
+   });
+
+   // client.on('mensaje', (payload) => {
+   //    console.log('Mensaje: ', payload);
+
+   //    io.emit('mensaje', { admin: 'Nuevo mensaje' });
+   // });
+
+   // client.on('emitir-mensaje', (payload) => {
+   //    // io.emit('nuevo-mensaje', payload); // emite a todos 
+   //    client.broadcast.emit('nuevo-mensaje', payload); // emite a todos menos al que lo emitio
+   // });
+
+   client.on('vote-band', (payload) => {
+      bands.voteBand(payload.id);
+      io.emit('active-bands', bands.getBands());
+   });
+
+   client.on('add-band', (payload) => {
+      bands.addBand(new Band(payload.name));
+      io.emit('active-bands', bands.getBands());
+   });
+
+   client.on('delete-band', (payload) => {
+      bands.deleteBand(payload.id);
+      io.emit('active-bands', bands.getBands());
+   });
 });
